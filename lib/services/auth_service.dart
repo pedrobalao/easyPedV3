f39d3c2943ab450bb763cc76a88ba063564799c4
Pedro Rocha <pedrocha@gmail.com>
2022-06-02 16:28:28 +0100

    fixed token

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class UserNotAuthenticated implements Exception {
   final String cause = "User needs to login";
@@ -22,11 +23,10 @@ class AuthenticationService {
 
     String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
 
-    String bearerToken = "bearer" + (token ?? (throw UserNotAuthenticated));
+    String? bearerToken = "bearer" + (token ?? (throw UserNotAuthenticated));
     // ignore: todo
     //TODO remove it
-    bearerToken =
-        "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTU0OTExMTE5N30.Ao7gDvxtuoMVyrsfeG3G6LDMZwWuIhbb4gJC5IL5Hus";
-    return bearerToken;
+    bearerToken = dotenv.env['TEST_TOKEN'];
+    return bearerToken!;
   }
 }
